868e1390e5d88af44d7bf205b02d44d579ee3b22
YSRKEN <ysr.ken@gmail.com>
2016-03-13 17:52:07 +0900

    ビットボードを使うQuery2型を実装中(諦め気味)

--- source/solve.cpp
+++ source/solve.cpp
@@ -1,6 +1,7 @@
 ﻿/* SweepOptimizer */
 
 #include <array>
+#include <cstdint>
 #include <chrono>
 #include <fstream>
 #include <iomanip>
@@ -10,12 +11,13 @@
 #include <tuple>
 #include <vector>
 
+using std::array;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
-enum Floor : size_t{
+enum Floor : uint_fast8_t {
 	Dirty,		//拭かれていない床
 	Clean,		//拭いた床
 	Boy,		//男の子
@@ -27,6 +29,7 @@ enum Floor : size_t{
 	DustBox,	//ゴミ箱(リンゴの捨て場所)
 	RecycleBox,	//リサイクル箱(ビンの捨て場所)
 	Obstacle,	//障害物
+	FloorKinds,
 };
 
 struct Status {
@@ -39,7 +42,159 @@ struct Status {
 	size_t stock_;			//リンゴ・ビンの所持数
 };
 
-const size_t kCleanerTypes = 3;	//掃除人の種類数(男の子・女の子・ロボット)
+// 掃除人の種類数(男の子・女の子・ロボット)
+const size_t kCleanerTypes = 3;
+// 最大の盤面サイズ
+const uint_fast8_t kFloorSizeX = 11, kFloorSizeY = 11;
+
+class BitBoard {
+	/* 盤面サイズの最大は9x9であり、番兵を加えても11x11である。
+	 * したがって、11*11=121ビットのビットボードをFloorの種類だけ保持すれば良い。
+	 * また、煩雑な処理を避けるため、ボードの上の方からboard_[0]→board_[1]と使い、
+	 * それぞれでは下位ビットの方から使用することにした。
+	 */
+	uint64_t board_[2];
+public:
+	bool GetBit(const size_t k) const noexcept {
+		if (k < 64) {
+			return (board_[0] & (1 << k) != 0);
+		}
+		else {
+			return (board_[1] & (1 << (k - 64)) != 0);
+		}
+	}
+	void SetZero() noexcept { board_[0] = 0; board_[1] = 0; }
+	void SetFull() noexcept { board_[0] = -1; board_[1] = -1; }
+	void SetBit(const size_t k) noexcept {
+		if (k < 64) {
+			board_[0] |= 1 << k;
+		}else{
+			board_[1] |= 1 << (k - 64);
+		}
+	}
+};
+
+struct Status2 {
+	Floor type_;					//掃除人の種類
+	uint_fast8_t move_now_;			//現在の歩数
+	uint_fast8_t move_max_;			//最大歩数
+	uint_fast8_t position_now_;		//現在の位置
+	uint_fast8_t position_old_;		//過去の位置
+	uint_fast8_t position_first_;	//最初の位置
+	uint_fast8_t stock_;			//リンゴ・ビンの所持数
+};
+
+class Query2 {
+	// 盤面サイズ
+	uint_fast8_t x_, y_;
+	// 床の状態
+	array<BitBoard, FloorKinds> floor_;
+	// 掃除人の種類・現在の歩数・最大歩数・現在の位置・過去の位置
+	vector<Status2> cleaner_status_;
+	// 最大歩数の最大
+	uint_fast8_t max_depth_;
+	// 解答における、各掃除人の移動経路
+	vector<std::list<uint_fast8_t>> cleaner_move_;
+	// マスA→マスBへの最小移動歩数
+	array<array<uint_fast8_t, kFloorSizeY>, kFloorSizeX> min_cost_;
+public:
+	// コンストラクタ
+	Query2(const char file_name[]) {
+		std::ifstream fin(file_name);
+		// 盤面サイズを読み込む
+		size_t x, y;
+		fin >> x >> y;
+		x_ = x + 2, y_ = y + 2;
+		// 盤面データを読み込む
+		for (auto &it_f : floor_) {
+			it_f.SetZero();
+		}
+		floor_[Floor::Obstacle].SetFull();
+		array<vector<Status2>, kCleanerTypes> cleaner_status_temp;
+		for (size_t j = 0; j < y; ++j) {
+			for (size_t i = 0; i < x; ++i) {
+				size_t temp;
+				fin >> temp;
+				Floor temp2 = static_cast<Floor>(temp);
+				uint_fast8_t k = Position(i, j);
+				switch (temp2) {
+				case Floor::Boy:
+					cleaner_status_temp[Floor::Boy - Floor::Boy].push_back(Status2{ Floor::Boy, 0, 0, k, k, k, 0 });
+					temp2 = Floor::Clean;
+					break;
+				case Floor::Girl:
+					cleaner_status_temp[Floor::Girl - Floor::Boy].push_back(Status2{ Floor::Girl, 0, 0, k, k, k, 0 });
+					temp2 = Floor::Clean;
+					break;
+				case Floor::Robot:
+					cleaner_status_temp[Floor::Robot - Floor::Boy].push_back(Status2{ Floor::Robot, 0, 0, k, k, k, 0 });
+					temp2 = Floor::Clean;
+					break;
+				default:
+					break;
+				}
+				floor_[temp2].SetBit(k);
+			}
+		}
+		// 掃除人データを読み込み、反映させる
+		max_depth_ = 0;
+		for (size_t ti = 0; ti < kCleanerTypes; ++ti) {
+			size_t temp;
+			fin >> temp;
+			if (cleaner_status_temp[ti].size() != temp) {
+				cout << "問題データに誤りがあります." << endl;
+				throw;
+			}
+			for (size_t ci = 0; ci < cleaner_status_temp[ti].size(); ++ci) {
+				fin >> temp;
+				cleaner_status_temp[ti][ci].move_max_ = temp;
+				max_depth_ = std::max(max_depth_, uint_fast8_t(temp));
+			}
+		}
+		for (auto &it_t : cleaner_status_temp) {
+			for (auto &it_c : it_t) {
+				cleaner_status_.push_back(it_c);
+			}
+		}
+		cleaner_move_.resize(cleaner_status_.size());
+	}
+	// 状態を表示する
+	void Put() const noexcept {
+		cout << "横" << (x_ - 2) << "マス,縦" << (y_ - 2) << "マス" << endl;
+		const static string kStatusStr[] = { "□", "×", "♂", "♀", "Ｒ", "水", "実", "瓶", "ゴ", "リ", "■" };
+		for (size_t j = 1; j < y_ - 1; ++j) {
+			for (size_t i = 1; i < x_ - 1; ++i) {
+				cout << kStatusStr[GetFloor(Position(i, j))];
+			}
+			cout << endl;
+		}
+		const static string kCleanerTypeStr[] = { "男の子", "女の子", "Robot" };
+		for (auto &it_c : cleaner_status_) {
+			auto type = it_c.type_;
+			auto position = it_c.position_now_;
+			auto move_now = it_c.move_now_;
+			auto move_max = it_c.move_max_;
+			cout << kCleanerTypeStr[type - Floor::Boy] << GetPos(position) << "(" << int(move_now) << "/" << int(move_max) << ")歩 ";
+		}
+		cout << endl;
+		cout << "hoge\n";
+	}
+	// ビットボードにおける座標を計算する
+	uint_fast8_t Position(const uint_fast8_t i, const uint_fast8_t j) const noexcept {
+		return (j + 1) * kFloorSizeX + (i + 1);
+	}
+	// 指定したマスにおける状態を表示する
+	Floor GetFloor(const uint_fast8_t position) const noexcept {
+		for (uint_fast8_t fi = 0; fi < Floor::FloorKinds; ++fi) {
+			if (floor_[fi].GetBit(position)) return static_cast<Floor>(fi);
+		}
+		return Floor::Dirty;
+	}
+	// ヘルパー関数
+	string GetPos(const uint_fast8_t position) const {
+		return "[" + std::to_string(position % kFloorSizeX - 1) + "," + std::to_string(position / kFloorSizeX - 1) + "]";
+	}
+};
 
 class Query{
 	// 盤面サイズ
@@ -433,5 +588,7 @@ int main(int argc, char *argv[]){
 		query.ShowAnswer();
 		cout << "処理時間：" << std::chrono::duration_cast<std::chrono::milliseconds>(process_end_time - process_begin_time).count() << "[ms]\n" << endl;
 	}
+	Query2 query2(argv[1]);
+	query2.Put();
 	return 0;
 }
